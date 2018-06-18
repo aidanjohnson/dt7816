4abf48fc696d0e894bf5aedf483b28d7c6e63fb6
aidanjohnson <j.a.johnson@ieee.org>
2018-06-18 14:31:11 -0700

    libxml installed; prelim custom program

---
+++ libxml2-2.9.8/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/opt/ti-sdk-am335x-evm-07.00.00.00/board-support/linux-3.12.10-ti2013.12.01/include/lib"
+XML2_LIBS="-lxml2 -lz -llzma    -lm "
+XML2_INCLUDEDIR="-I/opt/ti-sdk-am335x-evm-07.00.00.00/board-support/linux-3.12.10-ti2013.12.01/include/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
