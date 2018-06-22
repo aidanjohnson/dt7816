b6f104a1243e599482db9bcd700dd87518ca9424
aidanjohnson <j.a.johnson@ieee.org>
2018-06-21 17:05:16 -0700

    script for updating and compressing INSTALL files

--- INSTALL_tar.sh
+++ INSTALL_tar.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+cd /opt/ti-sdk-am335x-evm-07.00.00.00/
 sudo cp /opt/ti-sdk-am335x-evm-07.00.00.00/example-applications/dt78xx-examples/bat-array/* /opt/ti-sdk-am335x-evm-07.00.00.00/INSTALL/
-sudo tar --exclude=INSTALL.tar.gz -czvf INSTALL.tar.gz /opt/ti-sdk-am335x-evm-07.00.00.00/INSTALL/*
+cd INSTALL
+sudo tar --exclude=INSTALL.tar.gz -czvf INSTALL.tar.gz *
