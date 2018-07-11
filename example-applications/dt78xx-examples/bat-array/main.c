8ef43529cee654add4b7b10cfaa3a75b117c7da1
Aidan Johnson <34667756+aidanjohnson@users.noreply.github.com>
2018-07-11 13:05:41 -0700

    updated (c) date

--- example-applications/dt78xx-examples/bat-array/main.c
+++ example-applications/dt78xx-examples/bat-array/main.c
@@ -4,27 +4,27 @@
 * asynchronously from the input stream and written to a AIFF file. See:
 * https://msdn.microsoft.com/en-us/library/windows/desktop/aa365683(v=vs.85).aspx
 * for additional details on (a)synchronous I/O.
- * 
- * (c) Aidan Johnson (johnsj96@uw.edu)
- * 05 July 2018
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * 
- * ============================================================================
- * Usage :
- *	See usage() below
- */
+* 
+* (c) Aidan Johnson (johnsj96@uw.edu)
+* 11 July 2018
+* 
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* 
+* ============================================================================
+* Usage :
+*	See usage() below
+*/
 
 #include <stdint.h>
 #include <stdio.h>
